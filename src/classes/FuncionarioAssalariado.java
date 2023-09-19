c041f955d63f6a60d292f3a9b0cce44b9519b204
Giovani <giovanifetzner01@gmail.com>
2023-09-19 14:57:09 -0300

    Criação da hierarquia de classes

--- src/classes/FuncionarioAssalariado.java
+++ src/classes/FuncionarioAssalariado.java
@@ -1,24 +1,24 @@
 package classes;
 
 public class FuncionarioAssalariado extends Funcionario{
-    private double salario = 700.00;
+    private double salarioSemanal = 700.00;
 
     public FuncionarioAssalariado(String nome, String CPF, double salario) {
-        this.salario = salario;
+        this.salarioSemanal = salario;
         this.nome = nome;
         this.CPF = CPF;
     }
 
-    public double getSalario() {
-        return salario;
+    public double getSalarioSemanal() {
+        return salarioSemanal *4;
     }
 
-    public void setSalario(double salario) {
-        this.salario = salario;
+    public void setSalarioSemanal(double salarioSemanal) {
+        this.salarioSemanal = salarioSemanal;
     }
 
     @Override
     public double getRendimentos() {
-        return salario;
+        return salarioSemanal;
     }
 }
